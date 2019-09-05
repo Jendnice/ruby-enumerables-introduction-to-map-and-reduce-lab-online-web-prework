fa6eecdb02dcc9d3d25a11ceb2927ab761da478d
Jennifer Nicely <pipeline38@hotmail.com>
2019-09-05 21:18:23 +0000

    Automatically backed up by Learn

--- notes.rb
+++ notes.rb
@@ -1,3 +1,33 @@
+def reduce_to_total(source_array, starting_point)
+  new_array = []
+
+  while source_array.length do
+    new_array.push(source_array[0] + source_array[1])
+  end
+  starting_point = 0
+  source_array = [1, 2, 3]
+  new_array
+end
+
+def reduce_to_total(source_array, starting_point)
+  new_array = []
+
+  new_array = [starting_point + source_array[0] + source_array[1] + source_array[2]]
+  
+  starting_point = 0
+  source_array = [1, 2, 3]
+  new_array
+end
+
+def reduce_to_total(source_array, starting_point)
+  starting_point = 0
+  source_array = [1, 2, 3]
+  source_array.reduce(:+)
+end
+
+
+(5..10).reduce(:+)
+
 # My Code here....
 
 def map_to_negativize(source_array)
@@ -46,9 +76,7 @@ def reduce_to_total(source_array, starting_point = 0)
 
   while source_array.length > counter do
     total += (source_array[counter])
-    counter += 1 
   end
   total
 end
-
 
